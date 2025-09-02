fbe87dbad948412ae2ec77ed9e311ede5b61feec
chw <jilp1598.git@gmail.com>
2025-09-02 17:03:23 +0900

    feat: Tailwind CSS 스크롤바 스타일링 추가 및 메시지 목록 스크롤 영역 개선

--- frontend/components/ChatInterface.tsx
+++ frontend/components/ChatInterface.tsx
@@ -171,9 +171,9 @@ export function ChatInterface({ selectedConversationId, onConversationSelect }: 
   }
 
   return (
-    <div className="flex-1 flex flex-col">
+    <div className="flex-1 flex flex-col h-full">
       {/* 메시지 목록 */}
-      <div className="flex-1 overflow-y-auto p-4">
+      <div className="messages-container p-4 min-h-0">
         {isLoading ? (
           <div className="flex items-center justify-center h-full">
             <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-primary-600"></div>
